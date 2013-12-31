705fc51c5ef6011e9d6fe0c683b63b5731ccf388
canha42 <sean@design.blog.br>
2013-12-31 11:36:11 -0200

    Fixed issue #54

--- ppibfi_pinterest.php
+++ ppibfi_pinterest.php
@@ -325,7 +325,6 @@ function pibfi_engine_menu() {
 
 //Show the config page for the plugin in dashboard
 if ( is_admin() ) {
-	//pibifi_check_content_width();
 	wp_enqueue_style( 'pibfi_pinterest', XCPIN_PATH.'ppibfi_config.css' );
 	if ( 'on' == get_option( 'ppibfi_opt_enable' ) ) add_action( 'add_meta_boxes', 'xcp_optin' ); //ppibfi_meta.php
 
