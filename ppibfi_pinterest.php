9c76f1db8946e5a5f963b3447b9738638339e040
canha42 <sean@design.blog.br>
2013-12-31 12:52:11 -0200

    Uninstaller added to plugin

--- ppibfi_pinterest.php
+++ ppibfi_pinterest.php
@@ -363,4 +363,26 @@ function xc_pin_install() {
 
 // Run hook:
 register_activation_hook( __FILE__, 'xc_pin_install' );
+
+/*
+=================
+	uninstall
+=================
+*/
+
+function pibfi_uninstall() {
+	if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+		exit ();
+
+		delete_option( 'ppibfi_pg_index');
+		delete_option( 'ppibfi_pg_single');
+		delete_option( 'ppibfi_pg_page');
+		delete_option( 'ppibfi_pg_cat');
+		delete_option( 'ppibfi_img_pinthis');
+		delete_option( 'ppibfi_opt_enable');
+		delete_option( 'ppibfi_content_width');
+		delete_option( 'pibfi_no_show_button');
+}
+
+register_uninstall_hook( __FILE__, $callback );
 ?>
